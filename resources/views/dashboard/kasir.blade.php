8b9798fbcb412d46d0662ba34ae4d49b0e7a6a54
vincentve42 <vincentve42@gmail.com>
2025-07-02 18:56:06 +0700

    Fix some ui Problem in Kasir.blade

--- resources/views/dashboard/kasir.blade.php
+++ resources/views/dashboard/kasir.blade.php
@@ -26,7 +26,7 @@
         
       <div class="flex sm:hidden justify-between items-center justify-items-center" >
       <div>
-        <button @click="open = !open; gg = !gg">
+        <button @click="open = !open">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-10 pl-2">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
@@ -35,12 +35,12 @@
       <div>
         
         
-         <a href="{{ route('ViewCart') }}"> <button @click="carts = !carts">
+          <button @click="carts = !carts; gg = !gg">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-black">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
       </svg>
       </button>
-        </a>  
+          
            
 
         
@@ -71,7 +71,7 @@
       
       <div class="sm:flex sm:justify-between flex">
         <div class=" ml-5 sm:mt-5 sm:ml-70 w-256 h-128 max-h-1024 " x-show="gg">
-        <div>
+        
           <div class="pl-5 flex items-center justify-items-center">
             <form action="{{ route('SearchProduct') }}" method="post">
               @csrf
